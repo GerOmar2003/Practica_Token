efeae4e23554714e2f0cf32092f2f1ca1a2fce33
GerOmar2003 <162381621+GerOmar2003@users.noreply.github.com>
2025-06-10 23:30:27 -0500

    db.php

--- db.php
+++ db.php
@@ -1,5 +1,11 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "token_db");
+$host = "sql10.freesqldatabase.com";
+$usuario = "sql10784141";
+$contrasena = "qJGyAkYjWf";
+$basedatos = "sql10784141";
+$puerto = 3306;
+
+$conn = new mysqli($host, $usuario, $contrasena, $basedatos, $puerto);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
